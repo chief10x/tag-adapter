5f55a30b5a3eb19c6ac0dfe4114ed38889792b3b
Amin Ahrabian <aahrabian@gmail.com>
2019-04-23 15:20:05 +0200

    Migrate to android-x.

--- app/src/main/java/dev/amin/tagadapterexample/MainActivity.kt
+++ app/src/main/java/dev/amin/tagadapterexample/MainActivity.kt
@@ -1,6 +1,6 @@
 package dev.amin.tagadapterexample
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
     }
 }
