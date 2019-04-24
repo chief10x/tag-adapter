c1054717cc187992156c4f27746dc3e08173d4dc
Amin Ahrabian <aahrabian@gmail.com>
2019-04-24 17:28:44 +0200

    Add Ready functionality to TagAdapter and Measure RecyclerView Width and set it on MeasureHelper.

--- tagadapter/src/main/java/dev/amin/tagadapter/MeasureHelper.kt
+++ tagadapter/src/main/java/dev/amin/tagadapter/MeasureHelper.kt
@@ -2,6 +2,7 @@ package dev.amin.tagadapter
 
 import android.view.ViewGroup
 import android.view.ViewTreeObserver
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.row_tag.view.*
 
 class MeasureHelper(
@@ -9,18 +10,20 @@ class MeasureHelper(
     private val count: Int
 ) {
 
-    private val measureMap = hashMapOf<Int, Int>()
+    // Contains the span count [position] => [spanCount]
+    private val spanMap = hashMapOf<Int, Float>()
 
-    init {
+    private var baseCell: Float = 0f
 
+    fun measureBaseCell(width: Int) {
+        baseCell = (width / 20).toFloat()
     }
 
-    fun shouldMeasure() = measureMap.size != count
+    fun shouldMeasure() = spanCount.size != count
 
     private fun cellMeasured() {
 
-        // I don't know why the HELL it's not working if I use the shouldMeasure!
-        if (!adapter.measuringDone && measureMap.size == count)
+        if (!adapter.measuringDone && !shouldMeasure())
             adapter.measuringDone = true
     }
 
@@ -37,7 +40,11 @@ class MeasureHelper(
                 val marginTotal =
                     (itemView.rowTitle.layoutParams as ViewGroup.MarginLayoutParams).marginStart * 2
 
-                measureMap[position] = itemView.rowTitle.width + marginTotal
+                // calculate the required span here and add it to spanCount.
+                // Also Create a reference to rowManager here and add the span
+
+                spanCount[position] =
+                    ((itemView.rowTitle.width + marginTotal) / baseCell)
 
                 cellMeasured()
 
--- tagadapter/src/main/java/dev/amin/tagadapter/TagAdapter.kt
+++ tagadapter/src/main/java/dev/amin/tagadapter/TagAdapter.kt
@@ -1,9 +1,11 @@
 package dev.amin.tagadapter
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -24,6 +26,8 @@ class TagAdapter(
             update()
     }
 
+    private var ready = false
+
     // Called when the MeasureHelper is done measuring.
     private fun update() {
 
@@ -38,19 +42,35 @@ class TagAdapter(
 
     override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
         super.onAttachedToRecyclerView(recyclerView)
+
         this.recyclerView = recyclerView.apply {
             visibility = View.INVISIBLE
+
+            viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+
+                    // Measure the BaseCell on MeasureHelper and Start the Adapter
+                    measureHelper.measureBaseCell(recyclerView.width)
+
+                    ready = true
+                    notifyDataSetChanged()
+
+                    recyclerView.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                }
+            })
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         Holder(LayoutInflater.from(parent.context).inflate(R.layout.row_tag, parent, false))
 
-    override fun getItemCount() = tagList.size
+    override fun getItemCount() = if (ready) tagList.size else 0
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
 
         holder.setData(tagList[position])
+
+        Log.e("Tag", "ViewHolder $position Bind")
 
         if (measureHelper.shouldMeasure())
             measureHelper.measure(holder, position)
